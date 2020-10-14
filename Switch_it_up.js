ac1b779d403198500acbaca5ff0bac5c86c5bb22
Marina Bakhmat <48866406+MBakhmat77@users.noreply.github.com>
2020-10-13 21:55:36 -0400

    Create Switch_it_up.js
    When provided with a number between 0-9, return it in words.
    
    Input :: 1
    
    Output :: "One".
    
    If your language supports it, try using a switch statement.

---
+++ Switch_it_up.js
@@ -0,0 +1,27 @@
+//https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+function switchItUp(number){
+  let n;
+switch (number){
+    case 0: n = 'Zero';
+    break;
+case 1: n = 'One';
+ break;  
+case 2: n = 'Two';
+break; 
+case 3: n = 'Three';
+break; 
+case 4: n = 'Four';
+break; 
+case 5: n = 'Five';
+break; 
+case 6: n = 'Six';
+break; 
+case 7: n = 'Seven';
+break; 
+case 8: n ='Eight';
+break;
+case 9: n = 'Nine';
+break;
+}
+return n;
+}
